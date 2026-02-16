e2a3aa31474bf5851d58e0ac03322dee9e9c35da
JosepDorronsoro <ivanuniam@gmail.com>
2026-02-16 20:35:41 +0100

    tsv 2 1-4

---
+++ tsv/TSV_P2_Ivan_Dominguez/ejercicio2_3_a.m
@@ -0,0 +1,52 @@
+% Ejercicio 3a
+
+clear all;
+close all;
+clc;
+
+% definimos el nuevo espacio con el 
+% retículo anterior:
+
+x = 0:(1/200):1.02-(1/200);
+y = 0:(0.1/200):1.03-(0.1/200);
+
+% y definimos la malla:
+
+[X, Y] = meshgrid(x, y);
+
+f1 = sin(40 * pi * X + 30 * pi * Y); 
+f2 = sin(120 * X + 90 * Y);
+
+% obtenemos las transformadas:
+
+F1=fftshift(fft2(f1)); % ffshift sirve para que el centro no esté en la esquina
+F2=fftshift(fft2(f2));
+
+% creamos los ejes de coordenadas "reales":
+
+image_w = length(x);
+image_h = length(y);
+
+fx=-image_w/2:image_w/2; fx=fx(1:end-1);
+fy=-image_h/2:image_h/2; fy=fy(1:end-1);
+
+% las pintamos en 2D antes de normalizar:
+
+figure; 
+imshow(log(1+abs(F1)),[]);
+
+figure; 
+imshow(log(1+abs(F2)),[]);
+
+% Ahora normalizamos los módulos de cada transformada:
+
+F1=abs(F1)/max(max(abs(F1)));
+F2=abs(F2)/max(max(abs(F2)));
+
+% y ahora en 3D:
+
+figure;
+mesh(fx, fy, F1);
+
+figure;
+mesh(fx, fy, F2);
---
+++ tsv/TSV_P2_Ivan_Dominguez/ejercicio2_4_columna.m
@@ -0,0 +1,42 @@
+% Ejercicio 4 - columna
+
+clear all;
+close all;
+clc;
+
+% cargamos la imagen del tobillo: 
+
+[ima, map] = imread("P2imagenes/Hernia_disco.jpg");
+
+% la pasamos a double:
+
+ima = double(ima);
+
+% calculamos su extension especular:
+
+fila_superior = [ima, fliplr(ima)];
+fila_inferior = flipud(fila_superior);
+ima_especular = [fila_superior; fila_inferior];
+
+% calculamos sus ft:
+
+FT_normal = fftshift(fft2(ima));
+FT_extendida = fftshift(fft2(ima_especular));
+
+% normalizamos las ft:
+
+%FT_normal=abs(FT_normal)/max(max(abs(FT_normal)));
+%FT_extendida=abs(FT_extendida)/max(max(abs(FT_extendida)));
+
+
+% vemos la ft de ambas imágenes:
+
+figure;
+subplot(2, 2, 1);
+imshow(ima, []);
+subplot(2, 2, 2);
+imshow(ima_especular, []);
+subplot(2, 2, 3);
+imshow(log(1+abs(FT_normal)), []);
+subplot(2, 2, 4);
+imshow(log(1+abs(FT_extendida)), []);
