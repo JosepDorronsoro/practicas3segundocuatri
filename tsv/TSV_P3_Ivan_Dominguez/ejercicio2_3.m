f43360b5c810e5b4476c45ed47c646df42da1056
JosepDorronsoro <ivanuniam@gmail.com>
2026-02-22 20:15:48 +0100

    tsv p3 1-3

---
+++ tsv/TSV_P3_Ivan_Dominguez/ejercicio2_3.m
@@ -0,0 +1,52 @@
+% Ejercicio 3
+
+clear all;
+close all;
+clc;
+
+% cargamos la imagen:
+[ima, map] = imread('P3imagenes/Skin_gray_bc_560.tif');
+
+% la transformamos a imagen de grises:
+ima_bw = ind2gray(ima, map); % (un solo canal)
+
+% aplicamos la modificacion de 
+% contraste que se pide:
+
+a=80;b=160;s_a=30;s_b=220;
+ima_contrast=modificarContraste(ima_bw, a, b, s_a, s_b);
+
+figure;
+subplot(2, 2, 1);
+imshow(ima);
+title('Skin_gray_bc_560.tif');
+subplot(2, 2, 2);
+imshow(ima_contrast);
+title('cambio de contraste de Skin_gray_bc_560.tif')
+subplot(2, 2, 3);
+[counts, binLocations]=imhist(ima);
+bar(binLocations, counts);
+subplot(2, 2, 4);
+[counts, binLocations]=imhist(ima_contrast);
+bar(binLocations, counts);
+
+% Ahora se pide deshacer el cambio. Para ello 
+% mantendremos s_a y s_b y disminuiremos el 
+% rango [b, a]
+
+a=256-90;b=256-70;s_a=150;s_b=180;
+ima_contrast_2=modificarContraste(ima_contrast, a, b, s_a, s_b);
+
+figure;
+subplot(2, 2, 1);
+imshow(ima);
+title('Skin_gray_bc_560.tif');
+subplot(2, 2, 2);
+imshow(ima_contrast_2);
+title('cambio de contraste de Skin_gray_bc_560.tif')
+subplot(2, 2, 3);
+[counts, binLocations]=imhist(ima);
+bar(binLocations, counts);
+subplot(2, 2, 4);
+[counts, binLocations]=imhist(ima_contrast_2);
+bar(binLocations, counts);
