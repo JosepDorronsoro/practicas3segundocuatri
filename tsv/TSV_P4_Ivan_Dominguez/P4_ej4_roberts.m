787a97d32e599ea120019411e70ada94d67d86bf
JosepDorronsoro <ivanuniam@gmail.com>
2026-02-28 19:49:15 +0100

    tsv p4 ejs1-4

---
+++ tsv/TSV_P4_Ivan_Dominguez/P4_ej4_roberts.m
@@ -0,0 +1,43 @@
+% Ejercicio 4 - Sobel
+
+clear all;
+close all;
+clc;
+
+% primero cargamos la imagen como venimos haciendo:
+
+[ima, map]=imread('P4imas&code/edificio_bw_512.bmp');
+ima=ind2gray(ima,map);
+ima=im2double(ima);
+
+% ahora definimos los filtros de contorno:
+
+s1 = ones(2,2);
+s1(1,:)=[1, 0]; s1(2,:)=[0 -1]; 
+
+s2 = ones(2,2);
+s2(1,:)=[0, 1]; s2(2,:)=[-1 0]; 
+
+% calculamos las imagenes de borde:
+
+ima_borde_1=imfilter(ima, s1);
+ima_borde_2=imfilter(ima, s2);
+
+% la intensidad del borde se calcularía como:
+
+F1 = imfilter(ima,s1);
+F2 = imfilter(ima,s2);
+F  = sqrt(F1.^2 + F2.^2);
+
+% visualizamos con imagesc
+
+figure;
+subplot(1, 4, 1); imshow(ima);
+title('Imagen Original');
+subplot(1, 4, 2); colormap("gray"); imagesc(ima_borde_1);
+axis square; title('Bordes horizontales (x)')
+subplot(1, 4, 3); colormap("gray"); imagesc(ima_borde_2);
+axis square; title('Bordes verticales (y)')
+subplot(1, 4, 4); colormap("gray"); imagesc(F);
+axis square; title('Gradiente')
+
