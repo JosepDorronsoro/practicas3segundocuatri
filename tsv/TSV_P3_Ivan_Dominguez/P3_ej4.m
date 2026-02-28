3f6168bbfaf86925b6c1e3fe9a9c30a0ae721e49
JosepDorronsoro <ivanuniam@gmail.com>
2026-02-28 12:43:24 +0100

    tsv p3 ej5

--- tsv/TSV_P3_Ivan_Dominguez/P3_ej4.m
+++ tsv/TSV_P3_Ivan_Dominguez/P3_ej4.m
@@ -13,7 +13,7 @@ ima = rgb2gray(ima);
 
 % obtenemos la máscara y el umbral
 
-[mascara_global, umbral] = UmbralizaGlobal(ima);
+[mascara_global, ~] = UmbralizaGlobal(ima);
 
 % obtenemos la imagen umbralizada
 
@@ -45,7 +45,7 @@ bar(binLocations, counts);
 
 % obtenemos la máscara y el umbral
 
-[mascara_otsu, umbral] = UmbralizaGlobal(ima);
+[mascara_otsu, ~] = UmbralizaGlobal(ima);
 
 % obtenemos la imagen umbralizada
 
@@ -72,3 +72,34 @@ bar(binLocations, counts);
 subplot(2, 3, 6);
 [counts, binLocations]=imhist(ima_ostu);
 bar(binLocations, counts);
+
+% Ahora compararemos nuestros resultados con los de 
+% la funcion graythresh de matlab. 
+
+[umbral, a] = graythresh(ima); % solo devuelve el umbral
+umbral=umbral*255;
+mascara = ima>uint8(umbral); % creamos la máscara binaria
+ima_matlab=uint8(mascara).*ima; % nos quedamos con los valores 
+                         % donde la imagen es mayor que el umbral 
+
+% visualizamos                       
+figure; 
+subplot(2, 3, 1);
+imshow(ima_global);
+title(sprintf('Imagen umbralizada - Global, energía: %.3e', calcularEnergia(ima_global)));
+subplot(2, 3, 2);
+imshow(ima_ostu);
+title(sprintf('Imagen umbralizada - Otsu, energía: %.3e', calcularEnergia(ima_ostu)));
+subplot(2, 3, 3);
+imshow(ima_matlab);
+title(sprintf('Imagen umbralizada - graythresh, energía: %.3e', calcularEnergia(ima_matlab)));
+subplot(2, 3, 4);
+[counts, binLocations]=imhist(ima_global); 
+bar(binLocations, counts); axis tight;
+subplot(2, 3, 5);
+[counts, binLocations]=imhist(ima_ostu);
+bar(binLocations, counts);
+subplot(2, 3, 6);
+[counts, binLocations]=imhist(ima_matlab);
+bar(binLocations, counts);
+
