f7880d5c1d015559ca69a0d563bc1ee0b8788632
JosepDorronsoro <ivanuniam@gmail.com>
2026-03-15 10:26:43 +0100

    apaut3 p2 ej5 checkpoint

--- apaut3/AA3_2025_2026_P01_12_Miranda_Dominguez/models/density_methods.py
+++ apaut3/AA3_2025_2026_P01_12_Miranda_Dominguez/models/density_methods.py
@@ -1,4 +1,5 @@
 from sklearn.cluster import DBSCAN, HDBSCAN
+import numpy as np
 
 def train_dbscan(X, eps=0.5, min_samples=5):
     """Trains DBSCAN to find clusters of arbitrary shapes.
@@ -12,12 +13,14 @@ def train_dbscan(X, eps=0.5, min_samples=5):
     Returns:
         numpy.ndarray: Cluster labels. Noise points are labeled as -1.
     """
+    
     # DBSCAN is deterministic, so it does not require a random_state
     db = DBSCAN(eps=eps, min_samples=min_samples) 
-    labels = db.fit(X)
+    labels = db.fit_predict(X)
     return labels
 
 def train_hdbscan(X, min_cluster_size=5):
+    
     """Trains HDBSCAN (Hierarchical DBSCAN).
 
     Ideal for datasets with variable densities. It does not require a fixed eps.
@@ -34,3 +37,19 @@ def train_hdbscan(X, min_cluster_size=5):
     hdb = HDBSCAN(min_cluster_size)
     labels = hdb.fit(X)
     return labels.labels_
+
+def calculate_wcss(X, labels):
+    """Manually calculates WCSS, ignoring noise points (-1)"""
+    wcss = 0
+    # Find all unique clusters, ignoring noise (-1)
+    clusters = [label for label in np.unique(labels) if label != -1]
+    
+    for cluster in clusters:
+        # Get all points in this cluster
+        cluster_points = X[labels == cluster]
+        # Find the center (centroid) of this specific cluster
+        centroid = np.mean(cluster_points, axis=0)
+        # Add the squared distances from points to the centroid
+        wcss += np.sum((cluster_points - centroid) ** 2)
+        
+    return wcss
